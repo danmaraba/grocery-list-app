f927a9c1f53f51c287ab48c339ea85bd5e99def5
danmaraba <danielkmaraba@gmail.com>
2024-06-27 18:45:05 +0300

    add handlefilter function

--- Grocery-list-app/src/components/ShoppingList.jsx
+++ Grocery-list-app/src/components/ShoppingList.jsx
@@ -1,12 +1,26 @@
-import React,{useState}from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
-function ShoppingList(){
-    return(
-        <>
-        <Item/>
-        </>
-    )
+function ShoppingList() {
+  const [SelectedCategory, SetSelectedCategory] = useState("All");
+  function handleFilter(e) {
+    SetSelectedCategory(e.target.value);
+  }
+  return (
+    <div className="flex text-2xl rounded-md">
+      <select
+        name="filter"
+        className="bg-[#3498DB] w-[100%] my-4"
+        onChange={handleFilter}
+      >
+        <option value="All">Filter By Category</option>
+        <option value="Dairy">Dairy</option>
+        <option value="Produce">Produce</option>
+        <option value="Dessert">Dessert</option>
+      </select>
+      <Item />
+    </div>
+  );
 }
 
-export default ShoppingList
+export default ShoppingList;
