0726017c71ac15f039ffb2d231bf98db97dc4b93
danmaraba <danielkmaraba@gmail.com>
2024-06-27 18:19:12 +0300

    add ternary operator

--- Grocery-list-app/src/components/App.jsx
+++ Grocery-list-app/src/components/App.jsx
@@ -8,11 +8,13 @@ function App() {
     setIsDark(!isDark)
   }
 
+  const appClass=isDark? "App dark": "App light"
+
   return (
-    <div className='app'>
-     <header className='flex mx-4  items-center text-3xl'>
+    <div className={appClass}>
+     <header className='flex items-center text-3xl justify-between mx-96 shadow-lg mt-4 p-4'>
       <h1 className=''>Shopster Shop</h1>
-      <button onClick={toggleMode} className='bg-yellow-400 p-2 m-2 rounded-md'>{isDark? "Dark Mode":"Light Mode"}</button>
+      <button onClick={toggleMode} className='bg-yellow-400 p-2 m-2 rounded-md transition-all'>{isDark? "Dark Mode":"Light Mode"}</button>
      </header>
       <ShoppingList/>
     </div>
