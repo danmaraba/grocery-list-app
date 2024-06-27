b2df9906a956f6c0414df79ee405032cde4f585b
danmaraba <danielkmaraba@gmail.com>
2024-06-27 15:21:53 +0300

    create itemData

---
+++ Grocery-list-app/src/data/item.jsx
@@ -0,0 +1,8 @@
+const itemData=[
+    { id: 1, name: "Yogurt", category: "Dairy" },
+    { id: 2, name: "Pomegranate", category: "Produce" },
+    { id: 3, name: "Lettuce", category: "Produce" },
+    { id: 4, name: "String Cheese", category: "Dairy" },
+    { id: 5, name: "Cookies", category: "Dessert" },
+]
+export default itemData
