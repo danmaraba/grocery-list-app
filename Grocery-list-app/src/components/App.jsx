6211587926a354e4cdb166aa51dfe1ca3c614224
danmaraba <danielkmaraba@gmail.com>
2024-06-27 16:49:59 +0300

    add an onclick event

--- Grocery-list-app/src/components/App.jsx
+++ Grocery-list-app/src/components/App.jsx
@@ -4,11 +4,15 @@ import ShoppingList from './ShoppingList';
 function App() {
   const [isDark, setIsDark] = useState(false);
 
+  function toggleMode(){
+    setIsDark(!isDark)
+  }
+
   return (
     <div className='app'>
-     <header className='flex mx-4 text-green-400 items-center text-3xl'>
+     <header className='flex mx-4  items-center text-3xl'>
       <h1 className=''>Shopster Shop</h1>
-      <button>Click me</button>
+      <button onClick={toggleMode} className='bg-yellow-400 p-2 m-2 rounded-md'>{isDark? "Dark Mode":"Light Mode"}</button>
      </header>
       <ShoppingList/>
     </div>
