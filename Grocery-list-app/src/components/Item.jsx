e7ce9a0c3b759d0ef6cc29b8fd4d70b023a7ebfb
danmaraba <danielkmaraba@gmail.com>
2024-06-28 19:42:57 +0300

    add an onclick event

--- Grocery-list-app/src/components/Item.jsx
+++ Grocery-list-app/src/components/Item.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
 
-function Item(){
+function Item({name, category}){
+   const[inCart, setInCart]= useState(false)
+   const itemsClass=inCart? "in-Cart": ""
+//    inCart? "Add To Cart":"Remove From Cart"
+   function handleClick(){
+    setInCart(!inCart)
+   }
     return(
-        <></>
+        <div className="flex">
+            <li className={itemsClass}>
+                <span className="mr-16">{name}</span>
+                <span className="mr-16">{category}</span>
+                <button onClick={handleClick} >{inCart? "Add To Cart":"Remove From Cart"}</button>
+            </li>
+        </div>
     )
 }
 
