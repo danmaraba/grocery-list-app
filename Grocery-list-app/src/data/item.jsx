2a5b09cdd92419d0563b7695ccef32451ed2e37c
danmaraba <danielkmaraba@gmail.com>
2024-06-28 19:43:35 +0300

    create an array of objects

--- Grocery-list-app/src/data/item.jsx
+++ Grocery-list-app/src/data/item.jsx
@@ -1,8 +1,8 @@
-const itemData=[
+const items=[
     { id: 1, name: "Yogurt", category: "Dairy" },
     { id: 2, name: "Pomegranate", category: "Produce" },
     { id: 3, name: "Lettuce", category: "Produce" },
-    { id: 4, name: "String Cheese", category: "Dairy" },
+    { id: 4, name: "Cheese", category: "Dairy" },
     { id: 5, name: "Cookies", category: "Dessert" },
 ]
-export default itemData
+export default items
