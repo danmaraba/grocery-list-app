06bc5f4a877f01c1afed897624fee738b08285b6
danmaraba <danielkmaraba@gmail.com>
2024-06-28 19:42:23 +0300

    add an onlick event

--- Grocery-list-app/src/components/App.jsx
+++ Grocery-list-app/src/components/App.jsx
@@ -1,24 +1,32 @@
-import React, { useState } from 'react'
-import ShoppingList from './ShoppingList';
+import React, { useState } from "react";
+import ShoppingList from "./ShoppingList";
+import itemData from "../data/item";
 
 function App() {
   const [isDark, setIsDark] = useState(false);
 
-  function toggleMode(){
-    setIsDark(!isDark)
+  function toggleMode() {
+    setIsDark(!isDark);
   }
-
-  const appClass=isDark? "App dark": "App light"
+  // add dark &light mode
+  const appClass = isDark ? "App dark" : "App light";
 
   return (
     <div className={appClass}>
-     <header className='flex items-center text-3xl justify-between mx-96 shadow-lg mt-4 p-4'>
-      <h1 className=''>Shopster Shop</h1>
-      <button onClick={toggleMode} className='bg-yellow-400 p-2 m-2 rounded-md transition-all'>{isDark? "Dark Mode":"Light Mode"}</button>
-     </header>
-      <ShoppingList/>
+      <div className="text-2xl  mx-96 shadow-lg mt-4 p-4 w-1/3 place-items-center">
+        <header className="flex items-center justify-between ">
+          <h1 className="font-bold">Shopster Shop</h1>
+          <button
+            onClick={toggleMode}
+            className="bg-[#F5B041] p-2 m-2 rounded-md transition-all"
+          >
+            {isDark ? "Dark Mode" : "Light Mode"}
+          </button>
+        </header>
+        <ShoppingList item={itemData} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
