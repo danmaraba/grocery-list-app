78c9f7e26997046f5bbffd26cbb4b4228447ec5f
danmaraba <danielkmaraba@gmail.com>
2024-06-28 19:43:16 +0300

    add an onchange event

--- Grocery-list-app/src/components/ShoppingList.jsx
+++ Grocery-list-app/src/components/ShoppingList.jsx
@@ -1,24 +1,52 @@
 import React, { useState } from "react";
 import Item from "./Item";
+import items from "../data/item";
 
 function ShoppingList() {
-  const [SelectedCategory, SetSelectedCategory] = useState("All");
-  function handleFilter(e) {
-    SetSelectedCategory(e.target.value);
+  // initialize usestate and assign items data
+  const [shoppings, setShoppings] = useState(items);
+  const [filterBy, setFilterBy] = useState("All");
+  // console.log(shopping);
+
+  // update the <select> element to set the filterBy variable when its value is changed
+  function handleFilterChange(e) {
+    setFilterBy(e.target.value);
   }
+
+  // filter by category
+  const itemsToDisplay = shoppings.filter((shopping) => {
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return shopping.category === filterBy;
+    }
+  });
+
+  // display items data
+  // const displayItems = itemsToDisplay.map((item) => (
+  //   <li >
+      
+  //   </li>
+  // ));
   return (
-    <div className="flex text-2xl rounded-md">
+    <div className="text-2xl rounded-md">
       <select
         name="filter"
-        className="bg-[#3498DB] w-[100%] my-4"
-        onChange={handleFilter}
+        className="bg-[#3498DB] w-[100%] h-16 ml-1 text-sm"
+        onChange={handleFilterChange}
       >
         <option value="All">Filter By Category</option>
         <option value="Dairy">Dairy</option>
         <option value="Produce">Produce</option>
         <option value="Dessert">Dessert</option>
       </select>
-      <Item />
+      <ul>
+        <li className="mt-18 grid gap-8">
+        {itemsToDisplay.map((item) => (
+        <Item key={item.key} name={item.name}category={item.category}/>
+        ))}
+        </li>
+      </ul>
     </div>
   );
 }
